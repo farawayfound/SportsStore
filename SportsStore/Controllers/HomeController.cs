e6fcdc58034e896040673497c6e0a706594c6321
David <david.chui@outlook.com>
2021-04-07 10:24:28 -0600

    corrected NULL reference error

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace SportsStore.Controllers
         // F i e l d s  &  P r o p e r t i e s
 
         private IProductRepository _repository;
+        private int _pageSize = 3;
 
         // C o n s t r u c t o r s
         public HomeController(IProductRepository repository) // Dependency Injection - DI
@@ -17,14 +18,21 @@ namespace SportsStore.Controllers
         }
 
         // M e t h o d s
-        public IActionResult Index()
-        {
+        //public IActionResult Index() 
+        //    => View(_repository.GetAllProducts());
+        
             //// 1. Go to database and get data out.
             //IQueryable<Product> allProducts = _repository.GetAllProducts();
             //// 2. Send that data to the view
             //return View(allProducts);
 
-            return View(_repository.GetAllProducts());
+        public IActionResult Index(int productPage = 1)
+        {
+            IQueryable<Product> allProducts = _repository.GetAllProducts();
+            IQueryable<Product> someProducts = allProducts.OrderBy(p => p.ProductId)
+                                                          .Skip((productPage - 1) * _pageSize)
+                                                          .Take(_pageSize);
+            return View(someProducts);                                              
         }
     }
 }
